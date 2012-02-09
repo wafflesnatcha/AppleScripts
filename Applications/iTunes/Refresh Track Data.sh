370af2552b8f88eedb8c4748e62b2dc4aa33d933
Scott <buchanan.sc@gmail.com>
2012-02-09 03:28:42 -0800

    Refresh Track Data.sh now runs in the background

--- Applications/iTunes/Refresh Track Data.sh
+++ Applications/iTunes/Refresh Track Data.sh
@@ -7,12 +7,11 @@ input=$(osascript -ss -e 'tell application "iTunes" to return selection as list'
 
 total_lines=$(echo "$input" | wc -l)
 
-echo "$input" | { while read line; do
+( echo "$input" | { while read line; do
 	(( count++ ))
 	[[ "$line" = "" ]] && continue
-	
 	echo "$count/$total_lines*100" | bc -l | xargs printf "%1.0f%% "
 	osascript -sh -e "tell application \"iTunes\" to tell ${line} to return artist & \" - \" & name"
 	osascript -ss -e "tell application \"iTunes\" to tell ${line} to refresh"
-done; } | CocoaDialog progressbar --title "Refresh Track Data..."
+done; } | CocoaDialog progressbar --title "Refresh Track Data..."; ) &>/dev/null &
 
