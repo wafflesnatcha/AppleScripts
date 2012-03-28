ad3bc37827741e1b4d8d3cb6174a66cfe4d089a7
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-27 22:54:50 -0700

    * Refresh Track Data.sh: outputs to stdout when run from a command line (instead of always showing CocoaDialog)

--- Applications/iTunes/Refresh Track Data.sh
+++ Applications/iTunes/Refresh Track Data.sh
@@ -8,13 +8,14 @@ input=$(osascript -ss -e 'tell application "iTunes" to return selection as list'
 total_lines=$(echo "$input" | wc -l)
 
 cocoadialog="$(which CocoaDialog 2>/dev/null)"
+[[ $cocoadialog && ! $TERM =~ ^xterm* ]] && outcmd="$cocoadialog progressbar --title 'Refresh Track Data...'" || outcmd="cat -"
 
 ( echo "$input" | { while read line; do
 	(( count++ ))
 	[[ "$line" = "" ]] && continue
 	percent=$(echo "$count/$total_lines*100" | bc -l | xargs printf "%1.0f%%")
-	echo -n "$percent [$percent]  "
+	echo -n "$percent [$percent] "
 	osascript -sh -e "tell application \"iTunes\" to tell ${line} to return artist & \" - \" & name"
 	osascript -ss -e "tell application \"iTunes\" to tell ${line} to refresh"
-done; } | { [[ $cocoadialog ]] && "$cocoadialog" progressbar --title "Refresh Track Data..."; } ) &>/dev/null &
+done; } | $outcmd ) &
 
