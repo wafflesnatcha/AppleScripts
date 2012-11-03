a18391ab9611bc0412fd1f11d235ed68ab8f098d
Scott Buchanan <buchanan.sc@gmail.com>
2012-11-03 15:34:14 -0700

    prettier output

--- Applications/iTunes/Refresh Track Data.sh
+++ Applications/iTunes/Refresh Track Data.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash -l
 
 refresh() {
+	local input total count=0
+
 	input=$(osascript -ss -e 'tell application "iTunes" to return selection as list' | perl -pe 's/^{||}$//g; s/,\s*/\n/g; s/^[\s]*$//g')
-	total_lines=$(echo "$input" | wc -l)
-	count=0
+	total=$(echo "$input" | wc -l | awk '{print $1}')
+
 	echo "$input" | while read line; do
-		(( count++ ))
-		[[ "$line" = "" ]] && continue
-		echo "$count/$total_lines*100" | bc -l | xargs printf "%1.0f%% "
+		((count++))
+		[[ $1 ]] && echo $count $total | awk '{printf "%i%% ", $1/$2*100+.5}'
+		echo -n "[$count/$total] "
 		osascript -sh -e "tell application \"iTunes\" to tell ${line} to return artist & \" - \" & name"
 		osascript -ss -e "tell application \"iTunes\" to tell ${line} to refresh"
 	done
@@ -17,5 +19,5 @@ cocoadialog="$(which cocoaDialog 2>/dev/null)"
 if [[ ! $cocoadialog || $TERM =~ ^xterm ]]; then
 	refresh
 else
-	refresh | $cocoadialog progressbar --title "$(basename "$0" | sed -E 's/^(.*)\.[^\.]*$/\1/g')" --icon Music &
+	refresh --dialog | $cocoadialog progressbar --title "$(basename "$0" | sed -E 's/^(.*)\.[^\.]*$/\1/g')" --text "Starting..." --icon Music --stoppable &
 fi
