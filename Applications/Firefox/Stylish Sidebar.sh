1f8bdf69aa68d9cfe2653f366cb12a537dcc32dc
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-25 20:43:22 -0800

    scripts run in background now, and have a 5 second timeout

--- Applications/Firefox/Stylish Sidebar.sh
+++ Applications/Firefox/Stylish Sidebar.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-osascript <<EOF &>/dev/null
-activate application "Firefox"
-try
-    tell application "System Events" to tell application process "Firefox" to click menu item "Stylish" of first menu of menu item "Sidebar" of menu "View" of menu bar 1
-end try
+osascript <<EOF &>/dev/null &
+with timeout of 5 seconds
+	try
+		activate application "Firefox"
+		tell application "System Events" to tell application process "Firefox" to click menu item "Stylish" of first menu of menu item "Sidebar" of menu "View" of menu bar 1
+	end try
+end timeout
 EOF
