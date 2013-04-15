161f19ca55a847e21c43fa7ca4dd5af0ca809fa2
Scott Buchanan <buchanan.sc@gmail.com>
2013-04-14 18:19:25 -0700

    New scripts for Mediainfo <http://mediainfo.sourceforge.net>

---
+++ Applications/MediaInfo/View > Easy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+osascript <<APPLESCRIPT &>/dev/null &
+with timeout of 5 seconds
+	try
+		activate application "MediaInfo"
+		tell application "System Events" to tell application process "MediaInfo" to click menu item "Easy" of menu "View" of menu bar 1
+	end try
+end timeout
+APPLESCRIPT
